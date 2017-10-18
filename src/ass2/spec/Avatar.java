86ab525772d71724eb7d5e31f523da0960d98a70
FionaCLin <clin4614@uni.sydney.edu.au>
2017-10-18 22:37:34 +1100

    avatar cover fur

--- src/ass2/spec/Avatar.java
+++ src/ass2/spec/Avatar.java
@@ -2,16 +2,23 @@ package ass2.spec;
 
 import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.glu.GLUquadric;
 import com.jogamp.opengl.util.gl2.GLUT;
+
+import ass2.spec.Game.Model;
 
 public class Avatar {
 	private double[] myPos;
+	private Texture myTextures[] = new Texture[2];
 
-	public Avatar(double x, double y, double z) {
+	public Avatar(double x, double y, double z, Texture face, Texture body) {
 		myPos = new double[3];
 		myPos[0] = x;
 		myPos[1] = y;
 		myPos[2] = z;
+		myTextures[0] = face;
+		myTextures[1] = body;
 	}
 
 	public double[] getPosition() {
@@ -20,18 +27,33 @@ public class Avatar {
 
 	public void drawAvatar(GL2 gl, GLUT glut) {
 		gl.glPushMatrix();
-	
+		// Turn on OpenGL texturing.
 			gl.glTranslated(myPos[0], myPos[1], myPos[2]);
 			gl.glScaled(.25, .25, .25);
 			gl.glTranslated(0, 2.5, 0);
 			gl.glColor3f(102f / 255, 0, 51f / 255);
-			glut.glutSolidSphere(1.0, 10, 10);
+			// bind the texture
+			gl.glBindTexture(GL.GL_TEXTURE_2D, myTextures[1].getTextureId());
+
+			GLU glu = new GLU();
+            // the built in glut sphere does not have texture coordinates set
+            //glut.glutSolidSphere(1, 20, 20);
+            GLUquadric quadric = glu.gluNewQuadric();
+            glu.gluQuadricTexture(quadric, true);
+            glu.gluQuadricNormals(quadric, GLU.GLU_SMOOTH);
+            glu.gluSphere(quadric, 1, 10, 10);
+
+			
+//			glut.glutSolidSphere(1.0, 10, 10);
 			gl.glPushMatrix();
 				gl.glTranslated(0, -.5, 0);
 				gl.glRotated(90, 1, 0, 0);
 		
 				gl.glColor3f(102f / 255, 51f / 255, 0);
-				glut.glutSolidCylinder(0.5, 2, 20, 20);
+				glu.gluQuadricTexture(quadric, true);
+	            glu.gluQuadricNormals(quadric, GLU.GLU_SMOOTH);
+//				glut.glutSolidCylinder(0.5, 2, 20, 20);
+				glu.gluCylinder(quadric,.5,.5,2, 20, 20);
 			gl.glPopMatrix();
 		gl.glPopMatrix();
 
