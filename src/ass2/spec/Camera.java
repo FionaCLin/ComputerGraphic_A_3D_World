0fbb602c8c35551e3a0ea9fb771f3d1f99d49e56
FionaCLin <clin4614@uni.sydney.edu.au>
2017-10-21 00:42:23 +1100

    fix camera following

--- src/ass2/spec/Camera.java
+++ src/ass2/spec/Camera.java
@@ -12,15 +12,14 @@ public class Camera {
 	private Avatar person;
 	private double step = .1;
 	private double dstAbovePerson = 3;
-	private double dstAwayPerson = 4 ;
-	private double angleAroundPerson = 10;
+	private double dstFromPerson = 4;
+	private double angleAroundPerson = 0;
+	private boolean isfollow;
 
 	private Dimension size;
+	private double cameray;
 
-	public Camera(Dimension size) {
-		this.size = size;
-		camerax = size.getHeight() / 2;
-		cameraz = size.getWidth() + 5;
+	public Camera() {
 	}
 
 	/**
@@ -68,56 +67,37 @@ public class Camera {
 		this.cameraz = cameraz;
 	}
 
-	public void setCamera(double[][][] verties) {
+	public void setCamera() {
 		GLU glu = new GLU();
-		double[] eyes = { size.getHeight() / 2, size.getHeight() / 2, size.getWidth() + 2 };
-		double[] centre = { size.getHeight() / 2, 0.0, size.getWidth() / 2 };
-
-		eyes[0] = camerax;
-		eyes[1] = 3; // Minimum height of camera.
-		eyes[2] = cameraz;
-
-		// Find max height of nearby terrain points.
-		double radius = 2.;
-
-		for (int i = 0; i < verties.length; i++) {
-			for (int j = 0; j < 3; j++) {
-				double[] vertex = verties[i][j];
-				if (Math.abs(camerax - vertex[0]) < radius && Math.abs(cameraz - vertex[2]) < radius)
-					eyes[1] = Math.max(eyes[1], vertex[1] + 1.5);
-			}
-		}
-		// System.out.println("height = " + eyes[1]);
+		double[] eyes = person.getMyPos();
+		double[] centre = person.getMyPos();
 
 		// Compass direction.
 		double[] dir = { 0, 0, 0 };
 
-		if (person == null) {
+		if (!isfollow) {
 			dir[0] = Math.sin(Math.toRadians(angle));
-			dir[1] = -.5;
+			dir[1] = .5;
 			dir[2] = -Math.cos(Math.toRadians(angle));
 
 			centre[0] = eyes[0] + dir[0];
 			centre[1] = eyes[1] + dir[1];
 			centre[2] = eyes[2] + dir[2];
-			System.out.println(eyes[0] + "xxx angle " + angle);
 			System.out.println(dir[0] + " x " + dir[1] + " y " + dir[2] + " z " + angle + " angle");
 			System.out.println(" x " + centre[0] + " y " + centre[1] + " z " + centre[2] + " angle " + angle);
 		} else {
-			double[] camera = person.getMyPos(); // look at person
+			centre = person.getMyPos(); // look at person
 			dir[0] = Math.sin(Math.toRadians(angleAroundPerson));
-			dir[1] = -.5;
-			dir[2] = -Math.cos(Math.toRadians(angleAroundPerson));
+			dir[2] = Math.cos(Math.toRadians(angleAroundPerson));
 
-			// eyes = person.getMyPos(); // look at person
-			eyes[1] = camera[1] + this.dstAbovePerson;
-			eyes[2] = camera[2] + this.dstAwayPerson;
-			centre[0] = camera[0] + dir[0];
-			centre[1] = camera[1] + dir[1];
-			centre[2] = camera[2] + dir[2];
+			eyes[0] = person.getMyPos()[0] + dstFromPerson * dir[0]; // look at
+																		// person
+			eyes[1] = person.getMyPos()[1] + dstAbovePerson;
+			eyes[2] = person.getMyPos()[2] + dstFromPerson * dir[2];
 
 			System.out.println("xxx" + eyes[0] + " Y " + eyes[1] + " Z " + eyes[2] + angleAroundPerson);
-			System.out.println(dir[0] + " x " + dir[1] + " y " + dir[2] + " z " + angleAroundPerson + " angle");
+			// System.out.println(dir[0] + " x " + dir[1] + " y " + dir[2] + " z
+			// " + angleAroundPerson + " angle");
 			System.out.println(
 					" ####x " + centre[0] + " y " + centre[1] + " z " + centre[2] + " angle " + angleAroundPerson);
 		}
@@ -154,6 +134,11 @@ public class Camera {
 	 */
 	public void setPerson(Avatar person) {
 		this.person = person;
+		// set min height 
+		camerax = person.getMyPos()[0] + 1;
+		cameray = person.getMyPos()[1] + 3 ; 		// set min height 
+		cameraz = person.getMyPos()[2] + 1;
+
 	}
 
 	public void rightAngleAroundPerson() {
@@ -163,6 +148,14 @@ public class Camera {
 	public void leftAngleAroundPerson() {
 		angleAroundPerson = (angleAroundPerson - 10) % 360;
 
+	}
+
+	public void setFollow() {
+		isfollow = !isfollow;
+	}
+
+	public boolean isFollow() {
+		return isfollow;
 	}
 
 }
